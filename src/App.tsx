02b73b1eb33f693bc0b440e1a2e82e3b9943c94f
Muhammad Asif <asif88383@gmail.com>
2023-11-08 19:59:28 +0500

    setup tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import './globals.css'
 
 const App = () => {
-  return <div id='root'></div>
+  return (
+    <div>
+      <h1 className='text-3xl font-bold underline'>Hello Snapgram</h1>
+    </div>
+  )
 }
 
 export default App
