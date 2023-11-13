ad63e6638fbbc4ebe6926cab8dc11eb6ad2becfb
Muhammad Asif <asif88383@gmail.com>
2023-11-14 00:57:05 +0500

    started signup form

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -21,10 +21,83 @@ import { SignupValidation } from '@/lib/validation'
 import { useUserContext } from '@/context/AuthContext'
 
 const SignupForm = () => {
+  // const {toast} = useToast()
+  const navigate = useNavigate()
+  const { checkAuthUser, isLoading: isUserLoading } = useUserContext()
+
+  const form = useForm<z.infer<typeof SignupValidation>>({
+    resolver: zodResolver(SignupValidation),
+    defaultValues: {
+      name: '',
+      username: '',
+      email: '',
+      password: '',
+    },
+  })
+
+  // Queries
+  // const {mutateAsync: createUserAccount, isLoading: isCreatingAccount} = useCreateUserAccount()
+  // const {mutateAsync: signInAccount, isLoading: isSigningIn} = userSignInAccount()
+
+  // Handler
+  const handleSignup = async (user: z.infer<typeof SignupValidation>) => {
+    try {
+      // const newUser = await createUserAccount(user)
+
+      // if(!newUser){
+      //   toast({title: 'Sign up failed. Please try again.'})
+      //   return
+      // }
+
+      // const session = await signInAccount({
+      //   email: user.email,
+      //   password: user.password,
+      // })
+
+      // if(!session){
+      //   toast({title: 'Something went wrong. Please login your new account'})
+      //   navigate('/sign-in')
+      //   return
+      // }
+
+      const isLoggedIn = await checkAuthUser()
+
+      if (isLoggedIn) {
+        form.reset()
+
+        navigate('/')
+      } else {
+        // toast({title: 'Login failed. Please try again.'})
+
+        return
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
-    <div>
-      <Button>Button</Button>
-    </div>
+    <Form {...form}>
+      <div className='sm:w-420 flex-center flex-col'>
+        <img src='/assets/images/logo.svg' alt='logo' />
+
+        <h2 className='h3-bold md:h2-bold pt-5 sm:pt-12'>
+          Create a new account
+        </h2>
+
+        <p className='text-light-3 small-medium md:base-regular mt-2'>
+          To use snapgram, Please enter your details
+        </p>
+
+        <form
+          onSubmit={form.handleSubmit(handleSignup)}
+          className='flex flex-col gap-5 w-full mt-4'
+        >
+          <FormField
+          control={}
+        </form>
+      </div>
+    </Form>
   )
 }
 
