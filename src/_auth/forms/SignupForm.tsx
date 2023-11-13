c7fce59a2d26f0bd63aabf8377ccb1115dbc97ab
Muhammad Asif <asif88383@gmail.com>
2023-11-13 23:42:52 +0500

    signup form

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,4 +1,19 @@
+import * as z from 'zod'
+import { useForm } from 'react-hook-form'
+import { Link, useNavigate } from 'react-router-dom'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  Formlabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
+import Loader from '@/components/shared/Loader'
 
 const SignupForm = () => {
   return (
