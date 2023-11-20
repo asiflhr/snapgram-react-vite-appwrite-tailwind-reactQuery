6a63e2d7d56114213fd28f53fc3a9178fe252d1a
Muhammad Asif <asif88383@gmail.com>
2023-11-20 18:37:01 +0500

    delete  and update post queries

--- src/lib/react-query/queries.ts
+++ src/lib/react-query/queries.ts
@@ -113,3 +113,28 @@ export const useGetUserPosts = (userId?: string) => {
     enabled: !!userId,
   })
 }
+
+export const useUpdatePost = () => {
+  const queryClient = useQueryClient()
+  return useMutation({
+    mutationFn: (post: IUpdatePost) => updatePost(post),
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_POST_BY_ID, data?.$id],
+      })
+    },
+  })
+}
+
+export const useDeletePost = () => {
+  const queryClient = useQueryClient()
+  return useMutation({
+    mutationFn: ({ postId, imageId }: { postId?: string; imageId: string }) =>
+      deletePost(postId, imageId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+      })
+    },
+  })
+}
