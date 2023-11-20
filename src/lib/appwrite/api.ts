2c231e4da31b0c2bc168dff1396ad8169f396fe7
Muhammad Asif <asif88383@gmail.com>
2023-11-20 17:41:03 +0500

    list users

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -443,3 +443,32 @@ export async function getRecentPosts() {
     console.log(error)
   }
 }
+
+// ============================================================
+// USER
+// ============================================================
+
+// ============================== GET USERS
+export async function getUsers(limit?: number) {
+  const queries = [Query.orderDesc('$createdAt')]
+
+  if (limit) {
+    queries.push(Query.limit(limit))
+  }
+
+  try {
+    const users = await databases.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      queries
+    )
+
+    if (!users) throw Error
+
+    return users
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+// ============================== GET USER BY ID
