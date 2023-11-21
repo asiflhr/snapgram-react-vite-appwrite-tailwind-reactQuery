4cb3646ff60b3c270c081fab81552d4802c3f3a2
Muhammad Asif <asif88383@gmail.com>
2023-11-21 23:31:46 +0500

    fixed Explore page bug

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -3,28 +3,8 @@ import { useInView } from 'react-intersection-observer'
 
 import { Input } from '@/components/ui'
 import useDebounce from '@/hooks/useDebounce'
-import { GridPostList, Loader } from '@/components/shared'
+import { GridPostList, Loader, SearchResults } from '@/components/shared'
 import { useGetPosts, useSearchPosts } from '@/lib/react-query/queries'
-
-export type SearchResultProps = {
-  isSearchFetching: boolean
-  searchedPosts: any
-}
-
-const SearchResults = ({
-  isSearchFetching,
-  searchedPosts,
-}: SearchResultProps) => {
-  if (isSearchFetching) {
-    return <Loader />
-  } else if (searchedPosts?.documents.length > 0) {
-    return <GridPostList posts={searchedPosts?.documents} />
-  } else {
-    return (
-      <p className='text-light-4 mt-10 text-center w-full'>No results found</p>
-    )
-  }
-}
 
 const Explore = () => {
   const { ref, inView } = useInView()
@@ -51,6 +31,7 @@ const Explore = () => {
   const shouldShowSearchResults = searchValue !== ''
   const shouldShowPosts =
     !shouldShowSearchResults &&
+    posts &&
     posts.pages.every((item) => item.documents.length === 0)
 
   return (
@@ -100,7 +81,7 @@ const Explore = () => {
         ) : shouldShowPosts ? (
           <p className='text-light-4 mt-10 text-center w-full'>End of posts</p>
         ) : (
-          posts.pages.map((item, index) => (
+          posts?.pages.map((item, index) => (
             <GridPostList key={`page-${index}`} posts={item.documents} />
           ))
         )}
